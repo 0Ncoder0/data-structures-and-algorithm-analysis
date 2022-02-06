377b0cb6f6f8cd5b1cc2a2ffba0c38c4b9237935
unknown <lblinf.dk@foxmail.com>
2022-02-06 17:24:29 +0800

    优化-二叉堆

--- 006-优先队列（堆）/实现/二叉堆/index.ts
+++ 006-优先队列（堆）/实现/二叉堆/index.ts
@@ -1,47 +1,54 @@
 /** 二叉堆 */
 export class BinaryHeap {
-  private store: number[] = [0];
+  private store: number[] = [];
+
+  /** 交换下标所在的值 */
+  private swap(indexA: number, indexB: number) {
+    const temp = this.store[indexA];
+    this.store[indexA] = this.store[indexB];
+    this.store[indexB] = temp;
+  }
+
+  /** 向下冒泡 */
+  private heapDown(index: number) {
+    // undefined < 0 : false
+    const leftIndex = index * 2 + 1;
+    const rightIndex = index * 2 + 2;
+
+    const nextIndex =
+      this.store[rightIndex] < this.store[leftIndex] ? rightIndex : leftIndex;
+
+    if (this.store[nextIndex] < this.store[index]) {
+      this.swap(index, nextIndex);
+      this.heapDown(nextIndex);
+    }
+  }
+
+  /** 向上冒泡 */
+  private heapUp(index: number) {
+    if (index === 0) return;
+    const value = this.store[index];
+    const parentIndex = Math.floor((index - 1) / 2);
+    const parentValue = this.store[parentIndex];
+    if (value < parentValue) {
+      this.swap(index, parentIndex);
+      this.heapUp(parentIndex);
+    }
+  }
 
   /** 插入元素 */
   public insert(element: number): void {
-    let index = this.store.length;
-    for (;;) {
-      let parentIndex = Math.floor(index / 2);
-
-      if (element < this.store[parentIndex]) {
-        this.store[index] = this.store[parentIndex];
-        index = parentIndex;
-      } else {
-        this.store[index] = element;
-        break;
-      }
-    }
+    this.store.push(element);
+    this.heapUp(this.store.length - 1);
   }
 
   /** 删除并返回最小值 */
   public deleteMin(): number | null {
-    if (this.store.length === 1) return null;
-    let last = this.store[this.store.length - 1];
-    let first = this.store[1];
-    let index = 1;
-    for (;;) {
-      const leftIndex = index * 2;
-      const rightIndex = leftIndex + 1;
-      const leftElement = this.store[leftIndex];
-      const rightElement = this.store[rightIndex];
-
-      if (leftElement === undefined) {
-        this.store[index] = last;
-        this.store.pop();
-        break;
-      } else if (rightElement === undefined || leftElement < rightElement) {
-        this.store[index] = leftElement;
-        index = leftIndex;
-      } else {
-        this.store[index] = rightElement;
-        index = rightIndex;
-      }
-    }
-    return first;
+    if (this.store.length === 0) return null;
+    if (this.store.length === 1) return this.store.pop();
+    const min = this.store[0];
+    this.store[0] = this.store.pop();
+    this.heapDown(0);
+    return min;
   }
 }
